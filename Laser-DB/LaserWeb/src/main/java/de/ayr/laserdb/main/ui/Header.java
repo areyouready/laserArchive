b22e58d8c95d4e25e82e864cf706dc3b5bccf533
Sebastian Basner <sebastian.basner@gmx.de>
2012-07-04 12:14:11 +0200

    Logo eingebaut und kleinere Layoutprobleme gefixt (Header)

--- Laser-DB/LaserWeb/src/main/java/de/ayr/laserdb/main/ui/Header.java
+++ Laser-DB/LaserWeb/src/main/java/de/ayr/laserdb/main/ui/Header.java
@@ -1,45 +1,55 @@
 package de.ayr.laserdb.main.ui;
 
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 
 public class Header extends HorizontalLayout {
 
     public Header() {
-        
+
         setStyleName("Header-Style");
-        setHeight("44px");
-        
+        setHeight("66px");
+
         initUI();
     }
 
     private void initUI() {
-        
+
         setWidth("100%");
         Panel headerPanel = new Panel();
-//        headerPanel.setSizeFull();
+        // headerPanel.setSizeFull();
         headerPanel.setScrollable(false);
-//        headerPanel.setHeight("150");
+        // headerPanel.setHeight("150");
         headerPanel.setHeight("100%");
-        
+
+        // Standard Margin von 18px rund um Elemente in einem Panel entfernen
+        AbstractLayout headerPanelLayout = (AbstractLayout) headerPanel.getContent();
+        headerPanelLayout.setMargin(false);
+
+        // neues Image aus Theme Ordner einbinden
+        Embedded logoImage = new Embedded(null, new ThemeResource("img/laserarchive_logo.png"));
+        logoImage.setType(Embedded.TYPE_IMAGE);
+
         HorizontalLayout hLayout = new HorizontalLayout();
-        
-        Label titleLabel = new Label("LaserDisc Database");
-        //titleLabel.setHeight("50%");
-        hLayout.addComponent(titleLabel);
-        hLayout.setComponentAlignment(titleLabel, Alignment.MIDDLE_LEFT);
+
+        // Label titleLabel = new Label("LaserDisc Database");
+        // titleLabel.setHeight("50%");
+        hLayout.addComponent(logoImage);
+        hLayout.setComponentAlignment(logoImage, Alignment.MIDDLE_LEFT);
         hLayout.setWidth("100%");
-        
+
         Button btnLogout = new Button("Logout");
         hLayout.addComponent(btnLogout);
-        hLayout.setComponentAlignment(btnLogout, Alignment.BOTTOM_RIGHT);
-        
+        hLayout.setComponentAlignment(btnLogout, Alignment.MIDDLE_RIGHT);
+
         headerPanel.addComponent(hLayout);
-        
+
         addComponent(headerPanel);
     }
-    
+
 }
